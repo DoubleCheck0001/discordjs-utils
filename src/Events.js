58a1149d29e37fcc6e84b8d028f2d1a449edd82e
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-12-22 03:35:49 -0600

    Added roles event emitter

--- src/Events.js
+++ src/Events.js
@@ -1,9 +1,11 @@
 const { EventEmitter } = require("events");
 const commands = new EventEmitter;
+const roles = new EventEmitter;
 class Events extends EventEmitter {
     constructor() {
         super();
         this.commands = commands;
+        this.roles = roles;
     }
 }
 module.exports = { Events };
