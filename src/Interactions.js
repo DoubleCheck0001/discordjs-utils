efde2cbc11b302f7c55601f5525868172c3e7c76
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-12-22 02:35:56 -0600

    Fixed command registering

--- src/Interactions.js
+++ src/Interactions.js
@@ -3,15 +3,6 @@ class Interactions extends interactionsClient {
     constructor(token, clientID) {
         super(token,  clientID);
 
-        this.registerCommand = function(name, description, options, callback) {
-            name = name.toLowerCase();
-            const data = {name, description, options};
-            this.createCommand(data).then((commands) => {
-                callback(null, commands);
-            }).catch((err) => {
-                callback(err);
-            });
-        }
         this.listCommands = function(callback) {
             this.getCommands().then((commands) => {
                 callback(null, commands);
@@ -20,14 +11,42 @@ class Interactions extends interactionsClient {
                 callback(err);
             });
         }
+
+        //Store commands in a Map for easy access
+        this.commands = new Map();
+        this.listCommands((err, commands) => {
+            if (err) {
+                throw err;
+            } else {
+                for (let command of commands) {
+                    this.commands.set(command.name, command);
+                }
+            }
+        })
+
+        this.registerCommand = function(name, description, options, callback) {
+            name = name.toLowerCase();
+            const data = {name, description, options};
+            this.createCommand(data).then((command) => {
+                if (!this.commands.get(name))  { //Add command to Map if it does not exist
+                    this.commands.set(name, command);
+                }
+                callback(null, command);
+            }).catch((err) => {
+                callback(err);
+            });
+        }
         this.unregisterCommand = function(name, callback) {
             this.listCommands((err, commands) => {
                 if (err) {
                     callback(err);
                 } else {
-                    for (let command of commands.slash.all) {
+                    for (let command of commands) {
                         if (command.name === name) {
                             this.deleteCommand(command.id).then(() => {
+                                if (this.commands.get(name))  { //Remove command to Map if it exists
+                                    this.commands.delete(name);
+                                }
                                 callback(null, "Deleted slash command with name", command.name, "and ID", command.id);
                             }).catch((err) => {
                                 callback(err);
